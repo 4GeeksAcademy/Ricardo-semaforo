2b658981b48c0c59966c10c20c909ec5dee9aa69
Ricardo <rick_fm_95@hotmail.com>
2025-07-02 01:25:29 +0000

    :sparkles: Full creation

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const colors = ["red","yellow","green"]
+	
+	const [lightColor, setLightColor] = useState("yellow");
+	const [includePurple, setIncludePurple] = useState(false)
+
+	const trafficLightHeight = lightColor === "purple"? "350px":"200px";
+	const allColors = includePurple ? [...colors, "purple"] : colors;
+	
+	const changeColorButton = () => {
+		const actualColor = allColors.indexOf(lightColor);
+		const nextColor = (actualColor + 1) % allColors.length;
+		setLightColor(allColors[nextColor])
+	}
+
+	const purpleActive = () => {
+		setIncludePurple (true);
+		setLightColor("purple");
+	}
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+            <div className="cable"></div>
+			<div className="trafficlight" style={{height:trafficLightHeight}} >
+				<div className={"light red " + (lightColor =="red" ? "on" : "") } onClick={() => setLightColor("red")} ></div>
+				<div className={"light yellow " + (lightColor =="yellow" ? "on" : "") } onClick={() => setLightColor("yellow")}></div>
+				<div className={"light green " + (lightColor =="green" ? "on" : "") } onClick={() => setLightColor("green")}></div>
+				{includePurple && ( <div className={"light purple " + (lightColor =="purple" ? "on" : "")} onClick={() => setLightColor("purple")} >  </div> ) }
+			</div>
+			<div className="m-2">
+				<button onClick={changeColorButton} >Change Color</button>
+				<button onClick={purpleActive} > Change to Purple</button>
+			</div>
+			
 		</div>
 	);
 };
